183afa28a633acb81b3f4015d0aea53ebfa9c8db
Changhogod <younch8342@naver.com>
2023-03-25 01:24:08 +0900

    simple test

---
+++ src/Solution2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int sum = Arrays.stream(nums).sum();
+
+    }
+}
