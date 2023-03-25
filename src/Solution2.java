0311c978ad80b112d46ad66095ef296293daa42c
Changhogod <younch8342@naver.com>
2023-03-25 16:29:03 +0900

    simple test

--- src/Solution2.java
+++ src/Solution2.java
@@ -1,9 +1,18 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Solution2 {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
-        int sum = Arrays.stream(nums).sum();
+        String temp = Arrays.stream(nums).mapToObj(String::valueOf).collect(Collectors.joining());
+        String answer = Arrays.stream(temp.split("")).sorted((a, b) -> b.compareTo(a)).collect(Collectors.joining());
+        System.out.println(answer);
+
+        String s = "changho";
+        String[] sArray = s.split("");
+        String answer1 = Arrays.stream(sArray).sorted((a, b) -> b.compareTo(a)).collect(Collectors.joining());
+        System.out.println(answer1);
+
 
     }
 }
