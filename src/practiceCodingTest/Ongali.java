cf7793e651f66a82d9bc1035dc05a78f91e41943
Changhogod <younch8342@naver.com>
2023-04-01 02:53:46 +0900

    programmers solve problem3

--- src/practiceCodingTest/Ongali.java
+++ src/practiceCodingTest/Ongali.java
@@ -1,6 +1,15 @@
 package practiceCodingTest;
 
-public class Ongali {
+import java.util.Arrays;
 
+public class Ongali {
+    public static void main(String[] args) {
+        int [] numbers ={1,3,5,5};
+        int n = 5;
+        double avg = Arrays.stream(numbers).average().orElse(0);
+        int answer = (int)Arrays.stream(numbers).filter(e -> e == 5).count();
+        System.out.println(answer);
+
+    }
 
 }
