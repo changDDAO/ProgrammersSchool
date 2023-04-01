a4cfb4b73b663b7005824804a44be7d54ad7ffa9
Changhogod <younch8342@naver.com>
2023-04-02 01:35:25 +0900

    basic Test

--- src/practiceCodingTest/StringProblem.java
+++ src/practiceCodingTest/StringProblem.java
@@ -2,6 +2,7 @@ package practiceCodingTest;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StringProblem {
     public static void main(String[] args) {
@@ -12,6 +13,19 @@ public class StringProblem {
         String collect = Arrays.stream(ex1.split("")).sorted().collect(Collectors.joining());
         System.out.println("collect = " + collect);
         String [] temp = new String[10];
+
+        int [] numbers = {1,2,3,4,5};
+        numbers = Arrays.stream(numbers).map(v->v*2).toArray();
+        int asInt = IntStream.rangeClosed(0, 5).min().getAsInt();
+        System.out.println("asInt = " + asInt);
+        Character a = '4';
+
+
+        char a1 = 'A';
+        char a2 = 'a';
+        System.out.println((int)a1);
+
+
 
 
     }
