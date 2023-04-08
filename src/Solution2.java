4a68030ac88c8a5d5d168ce0229c1f394d7143aa
Changhogod <younch8342@naver.com>
2023-04-08 14:45:32 +0900

    simple test

--- src/Solution2.java
+++ src/Solution2.java
@@ -7,7 +7,15 @@ public class Solution2 {
         System.out.println(s.substring(0,1));
         StringBuilder sb = new StringBuilder(s);
         s = sb.reverse().toString();
-        System.out.println(s);
+
+        String [] temp = s.split("");
+        for (int i = 0; i < temp.length; i+=3) {
+            for (int j = i; j < j + 3; j++) {
+                if(j== temp.length-1)return;
+                System.out.print(temp[j]);
+            }
+            System.out.println();
+        }
 
     }
 }
