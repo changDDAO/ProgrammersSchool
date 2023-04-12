52a621a1710579e131a843de92c99d3e9435091b
Changhogod <younch8342@naver.com>
2023-04-12 16:42:37 +0900

    LV1 problem 예산

---
+++ src/lv1/예산.java
@@ -0,0 +1,21 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class 예산 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+
+            if (budget < 0) break;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
