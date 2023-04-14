84e78fd3b6c8ee7626b6541e9d592ea991536fd5
Changhogod <younch8342@naver.com>
2023-04-14 16:06:52 +0900

    solve lv1 크기가_작은_부분문자열

--- src/lv1/크기가_작은_부분문자열.java
+++ src/lv1/크기가_작은_부분문자열.java
@@ -24,4 +24,14 @@ public class 크기가_작은_부분문자열 {
         }
         return answer;
     }
+   /* public int solution(String t, String p) {
+        int pLength = p.length();
+        long pValue = Long.parseLong(p);
+        int answer = 0;
+        for (int i = 0; i <= t.length() - pLength; i++) {
+            long tValue = Long.parseLong(t.substring(i, i + pLength));
+            if (tValue <= pValue)
+                answer++;
+        }
+        return answer;*/ //다른풀이
 }
