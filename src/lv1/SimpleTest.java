e1cb2f890bd756ac744b0dae4c3586c55a7e856e
Changhogod <younch8342@naver.com>
2023-04-18 12:32:54 +0900

    lv1 명예의전당 use PriorityQueue

--- src/lv1/SimpleTest.java
+++ src/lv1/SimpleTest.java
@@ -1,8 +1,22 @@
 package lv1;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class SimpleTest {
     public static void main(String[] args) {
         String s = Integer.toBinaryString(9);
         System.out.println(s);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(3, Comparator.reverseOrder());
+        pq.offer(7);
+        pq.offer(2);
+        pq.offer(5);
+        pq.offer(1);
+
+
+        Integer remove = pq.remove();
+        System.out.println(pq.peek());
+        System.out.println(remove);
     }
 }
